8313a291276004735381483f665ef1940bbad355
pist0ler0 <matii33333@gmail.com>
2025-06-02 17:37:12 +0200

    Dodanie do Bazy wpisów bazowych

--- GuildRPG/Data/GuildRPGContext.cs
+++ GuildRPG/Data/GuildRPGContext.cs
@@ -13,6 +13,37 @@ namespace GuildRPG.Data
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+
+            modelBuilder.Entity<Mercenary>().HasData(
+                new Mercenary { Id = 1, Name = "Arthur", Level = 5, MaxHealth = 100, Damage = 15, CurrentHealth = 100, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 2, Name = "Lancelot", Level = 7, MaxHealth = 120, Damage = 20, CurrentHealth = 120, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 3, Name = "Paulina Heros", Level = 25, MaxHealth = 10000, Damage = 1500, CurrentHealth = 10000, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 4, Name = "Cieć Boss", Level = 99, MaxHealth = 100000, Damage = 100000, CurrentHealth = 100000, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 5, Name = "Zwykły Maciek", Level = 8, MaxHealth = 100, Damage = 15, CurrentHealth = 100, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 6, Name = "Kasztan ze wsi", Level = 1, MaxHealth = 100, Damage = 15, CurrentHealth = 100, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 7, Name = "Dres spod żabki", Level = 9, MaxHealth = 100, Damage = 15, CurrentHealth = 100, ExperiencePoints = 100, Gold = 42198 },
+                new Mercenary { Id = 8, Name = "Nie mam Więcej pomysłów", Level = 5, MaxHealth = 100, Damage = 15, CurrentHealth = 100, ExperiencePoints = 100, Gold = 42198 }
+            );
+
+            modelBuilder.Entity<Monster>().HasData(
+                new Monster { Id = 1, Name = "Smokuch", Health = 2000, Damage = 200 },
+                new Monster { Id = 2, Name = "Goblin", Health = 100, Damage = 5 },
+                new Monster { Id = 3, Name = "Paulina Wiedźma", Health = 800, Damage = 100 },
+                new Monster { Id = 4, Name = "Ork", Health = 500, Damage = 50 },
+                new Monster { Id = 5, Name = "Czerwony Ork", Health = 1500, Damage = 150 },
+                new Monster { Id = 6, Name = "Bestia z Groty", Health = 2000, Damage = 200 },
+                new Monster { Id = 7, Name = "Żul Mietek", Health = 10, Damage = 1 },
+                new Monster { Id = 8, Name = "Mamlambo", Health = 10000, Damage = 1000 },
+                new Monster { Id = 9, Name = "Diabeł", Health = 999999, Damage = 999999 }
+            );
+
+            modelBuilder.Entity<Quest>().HasData(
+                new Quest { Id = 1, Name = "Smocza Grota", Location = "Góry Mroczne",Description = "aaaaaaaaaaaaaaaaaaaa", Diff = Difficulty.uberHard, EnemyId=1},
+                new Quest { Id = 2, Name = "Goblińska Wioska", Location = "Las Cienia",Description = "BBBBBBBBBBBBBBBB" ,Diff = Difficulty.notSoEasy, EnemyId = 2}
+            );
+        }
 
         public DbSet<GuildRPG.Models.Mercenary> Mercenary { get; set; } = default!;
         public DbSet<GuildRPG.Models.Monster> Monster { get; set; } = default!;
--- GuildRPG/Migrations/20250602153312_Init.cs
+++ GuildRPG/Migrations/20250602153312_Init.cs
@@ -2,6 +2,8 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace GuildRPG.Migrations
 {
     /// <inheritdoc />
@@ -67,6 +69,46 @@ namespace GuildRPG.Migrations
                         principalTable: "Monster",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Mercenary",
+                columns: new[] { "Id", "CurrentHealth", "Damage", "ExperiencePoints", "Gold", "Level", "MaxHealth", "Name" },
+                values: new object[,]
+                {
+                    { 1, 100.0, 15.0, 100.0, 42198.0, 5, 100.0, "Arthur" },
+                    { 2, 120.0, 20.0, 100.0, 42198.0, 7, 120.0, "Lancelot" },
+                    { 3, 10000.0, 1500.0, 100.0, 42198.0, 25, 10000.0, "Paulina Heros" },
+                    { 4, 100000.0, 100000.0, 100.0, 42198.0, 99, 100000.0, "Cieć Boss" },
+                    { 5, 100.0, 15.0, 100.0, 42198.0, 8, 100.0, "Zwykły Maciek" },
+                    { 6, 100.0, 15.0, 100.0, 42198.0, 1, 100.0, "Kasztan ze wsi" },
+                    { 7, 100.0, 15.0, 100.0, 42198.0, 9, 100.0, "Dres spod żabki" },
+                    { 8, 100.0, 15.0, 100.0, 42198.0, 5, 100.0, "Nie mam Więcej pomysłów" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Monster",
+                columns: new[] { "Id", "Damage", "Health", "Name" },
+                values: new object[,]
+                {
+                    { 1, 200.0, 2000.0, "Smokuch" },
+                    { 2, 5.0, 100.0, "Goblin" },
+                    { 3, 100.0, 800.0, "Paulina Wiedźma" },
+                    { 4, 50.0, 500.0, "Ork" },
+                    { 5, 150.0, 1500.0, "Czerwony Ork" },
+                    { 6, 200.0, 2000.0, "Bestia z Groty" },
+                    { 7, 1.0, 10.0, "Żul Mietek" },
+                    { 8, 1000.0, 10000.0, "Mamlambo" },
+                    { 9, 999999.0, 999999.0, "Diabeł" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Quest",
+                columns: new[] { "Id", "Description", "Diff", "EnemyId", "Location", "Name", "RewardMoney", "RewardXP" },
+                values: new object[,]
+                {
+                    { 1, "aaaaaaaaaaaaaaaaaaaa", 5, 1, "Góry Mroczne", "Smocza Grota", 0.0, 0.0 },
+                    { 2, "BBBBBBBBBBBBBBBB", 3, 2, "Las Cienia", "Goblińska Wioska", 0.0, 0.0 }
                 });
 
             migrationBuilder.CreateIndex(
